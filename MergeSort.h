203ed9d83b09cf3f226fd3f6cbc2103796390053
zhechengQoo <zhechengli@gmail.com>
2023-04-16 10:28:31 +0800

    MergeSort Complete

--- MergeSort.h
+++ MergeSort.h
@@ -1,17 +1,17 @@
 void MergeSortRecursive(int *arr, int *reg, int start, int end);
-void MergeSort(int *arr);
+void MergeSort(int *arr, int len);
+void PrintOutput(int *arr, int len);
 
-
-void MergeSort(int *arr)
+void MergeSort(int *arr, int len)
 {
-    int len, start, end;
-    
-    len = sizeof(brr)/sizeof(*brr);
-    printf("%d\n", len);
+    int start, end;
+    int reg[len];
     start = 0;
     end = len;
-    int *reg = arr;
-    MergeSortRecursive(arr, reg, start, end);
+    for (int i = 0; i < len; i++)
+        reg[i] = 0;
+    MergeSortRecursive(arr, reg, start, end - 1);
+    // index final number is "end-1", but Recursive is index of arrar.
 }
 
 void MergeSortRecursive(int *arr, int *reg, int start, int end)
@@ -21,9 +21,44 @@ void MergeSortRecursive(int *arr, int *reg, int start, int end)
         return;
     int Lstart, Lend, Rstart, Rend;
     Lstart = start;
-    Lend = len / 2;
-    Rstart = len / 2 + 1;
+    Lend = start + len / 2;
+    Rstart = start + len / 2 + 1;
     Rend = end;
+    // printf("%d  %d ||  %d  %d\n", Lstart, Lend, Rstart, Rend);
     MergeSortRecursive(arr, reg, Lstart, Lend);
-    printf("%d  %d ||  %d\n", Lstart, Lend, len);
+    MergeSortRecursive(arr, reg, Rstart, Rend);
+    int index = start;
+    while (Lstart <= Lend && Rstart <= Rend)
+    {
+        if (arr[Lstart] <= arr[Rstart])
+        {
+            reg[index] = arr[Lstart];
+            Lstart++;
+            index++;
+        }
+        else
+        {
+            reg[index] = arr[Rstart];
+            Rstart++;
+            index++;
+        }
+
+        //Runoob:一行就...可以，但真的滿難的。
+        //reg[index++] = arr[Lstart] < arr[Rstart] ?  arr[Lstart++] : arr[Rstart++];
+    }
+    while (Lstart <= Lend)
+    {
+        reg[index] = arr[Lstart];
+        Lstart++;
+        index++;
+    }
+    while (Rstart <= Rend)
+    {
+        reg[index] = arr[Rstart];
+        Rstart++;
+        index++;
+    }
+    // save reg into arr , because arr is sorted arrar
+    for (int i = start; i < end + 1; i++)
+        arr[i] = reg[i];
 }
--- shellsort.c
+++ shellsort.c
@@ -3,7 +3,7 @@
 #include "MergeSort.h"
 
 void PrintOutput(int *arr, int len)
-{   
+{
     for (int i = 0; i < len; i++)
         printf("%d  ", arr[i]);
     printf("\n");
@@ -11,22 +11,18 @@ void PrintOutput(int *arr, int len)
 int main()
 {
     int arr[10] = {5, 4, 8, 6, 9, 1, 10, 3, 2, 7};
-    int len = 0;
-    len = (int)sizeof(arr) / sizeof(*arr);
-
-    int *prt = arr;
-    printf("%d\n",sizeof(prt));
+    int len = (int)sizeof(arr) / sizeof(*arr);
 
     printf("Before Srot :");
     PrintOutput(arr, len);
 
-    MergeSort(arr);
-
+    MergeSort(arr, len);
+    printf("Increasing sequence :");
+    PrintOutput(arr, len);
     return 0;
 
     /*
-    printf("Increasing sequence :");
-    PrintOutput(arr, len);
+
 ShellSortIncreasing(arr, len);
     ShellSortDescending(arr,len);
     */
